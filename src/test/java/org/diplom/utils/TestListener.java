a7e4e200c6bccb454252331e5fc0853e96dda6e3
ilya-bychyk <ilya.bychyk@mail.com>
2022-06-07 13:46:49 +0300

    add making Screenshot

--- src/test/java/org/diplom/utils/TestListener.java
+++ src/test/java/org/diplom/utils/TestListener.java
@@ -1,18 +1,23 @@
 package org.diplom.utils;
 
 import io.qameta.allure.Attachment;
+import org.apache.commons.io.FileUtils;
 import org.diplom.driver.DriverSingleton;
-import org.diplom.page.BasePage;
+import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import static io.qameta.allure.Allure.addAttachment;
 
-public class TestListener extends BasePage implements ITestListener {
+
+public class TestListener implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
@@ -66,8 +71,86 @@ public class TestListener extends BasePage implements ITestListener {
     }
 
     @Attachment(value = "Last screen state", type = "image/png")
-    private byte[] takeScreenshot() {
-        return ((TakesScreenshot) DriverSingleton.getInstance().getDriver())
-                .getScreenshotAs(OutputType.BYTES);
+    private void takeScreenshot() {
+        try {
+            File screenshotAs = ((TakesScreenshot) DriverSingleton.getInstance().getDriver()).getScreenshotAs(OutputType.FILE);
+            addAttachment("Screenshot", FileUtils.openInputStream(screenshotAs));
+            System.out.println("======================================== CREATING SCREENSHOT SUCCESS ========================================");
+        } catch (IOException | NoSuchSessionException e) {
+            System.out.println("======================================== CREATING SCREENSHOT FAILED ========================================");
+        }
     }
 }
+
+//
+//import io.qameta.allure.Attachment;
+//import java.util.concurrent.TimeUnit;
+//import org.openqa.selenium.NoSuchSessionException;
+//import org.openqa.selenium.OutputType;
+//import org.openqa.selenium.TakesScreenshot;
+//import org.openqa.selenium.WebDriver;
+//import org.testng.ITestContext;
+//import org.testng.ITestListener;
+//import org.testng.ITestResult;
+//import org.diplom.driver.DriverSingleton;
+//
+//
+//public class TestListener implements ITestListener {
+//
+//    @Override
+//    public void onTestStart(ITestResult iTestResult) {
+//        System.out.println(String.format(
+//                "======================================== STARTING TEST %s ========================================",
+//                iTestResult.getName()));
+//    }
+//
+//    @Override
+//    public void onTestSuccess(ITestResult iTestResult) {
+//        System.out.println(String.format(
+//                "======================================== FINISHED TEST %s Duration: %ss ========================================",
+//                iTestResult.getName(),
+//                getExecutionTime(iTestResult)));
+//    }
+//
+//    @Override
+//    public void onTestFailure(ITestResult iTestResult) {
+//        System.out.println(String.format(
+//                "======================================== FAILED TEST %s Duration: %ss ========================================",
+//                iTestResult.getName(),
+//                getExecutionTime(iTestResult)));
+//        takeScreenshot();
+//    }
+//
+//    @Override
+//    public void onTestSkipped(ITestResult iTestResult) {
+//        System.out.println(String.format(
+//                "======================================== SKIPPING TEST %s ========================================",
+//                iTestResult.getName()));
+//    }
+//
+//    @Override
+//    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+//
+//    }
+//
+//    @Override
+//    public void onStart(ITestContext iTestContext) {
+//
+//    }
+//
+//    @Override
+//    public void onFinish(ITestContext iTestContext) {
+//
+//    }
+//
+//    private long getExecutionTime(ITestResult iTestResult) {
+//        return TimeUnit.MILLISECONDS
+//                .toSeconds(iTestResult.getEndMillis() - iTestResult.getStartMillis());
+//    }
+//
+//    @Attachment(value = "Last screen state", type = "image/png")
+//    private byte[] takeScreenshot() {
+//        return ((TakesScreenshot) DriverSingleton.getInstance().getDriver())
+//                .getScreenshotAs(OutputType.BYTES);
+//    }
+//}
